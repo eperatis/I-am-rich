65fce0ad631092d2923a507035acc51b012ae603
Eperjesi Attila Dávid <eper.atis@gmail.com>
2020-01-27 11:17:16 +0100

    Initial Commit

---
+++ I-am-rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I-am-rich
+//
+//  Created by Eperjesi Attila on 2020. 01. 27..
+//  Copyright © 2020. Eperjesi Attila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
